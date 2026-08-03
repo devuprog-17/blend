cce45a790a311ac5881ecb99b8ae58f86f42036b
Devraj Pujar <devupujar62@gmail.com>
2026-08-03 23:23:45 +0530

    Add camera and render controls to SKM UI

--- addon/skm_cinematic_generator/ui.py
+++ addon/skm_cinematic_generator/ui.py
@@ -13,16 +13,18 @@ class SKM_PT_main_panel(Panel):
         layout = self.layout
         settings = context.scene.skm_settings
 
-        col = layout.column(align=True)
+        box = layout.box()
+        box.label(text="Scene Settings")
+        col = box.column(align=True)
         col.prop(settings, "sanctum_size")
         col.prop(settings, "pillar_height")
         col.prop(settings, "pillar_radius")
         col.prop(settings, "hero_wall_width")
         col.prop(settings, "hero_wall_height")
 
-        layout.separator()
-
-        col = layout.column(align=True)
+        box = layout.box()
+        box.label(text="Generate")
+        col = box.column(align=True)
         col.operator("skm.create_sanctum", icon='MESH_CUBE')
         col.operator("skm.setup_materials", icon='MATERIAL')
         col.operator("skm.setup_lighting", icon='LIGHT')
