92bee6020f7428588236be817d40a3ff59e35604
Devraj Pujar <devupujar62@gmail.com>
2026-08-04 22:03:27 +0530

    Add collection and cleanup helpers for SKM Cinematic Generator

--- addon/skm_cinematic_generator/utils.py
+++ addon/skm_cinematic_generator/utils.py
@@ -20,6 +20,19 @@ def link_object_to_collection(obj, collection):
             c.objects.unlink(obj)
 
 
+def remove_unused_data_blocks():
+    for collection in (bpy.data.meshes, bpy.data.lights, bpy.data.cameras, bpy.data.materials, bpy.data.curves):
+        for block in list(collection):
+            if block.users == 0:
+                collection.remove(block)
+
+
+def clear_scene_objects():
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete(use_global=False, confirm=False)
+    remove_unused_data_blocks()
+
+
 def new_principled_material(name, base_color=(0.2, 0.2, 0.2, 1.0), roughness=0.7, metallic=0.0):
     mat = bpy.data.materials.get(name)
     if mat is None:
